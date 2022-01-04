7e198a499a1427786018fed839435559b4ce9b30
LeonardoVieiraNeto <neto.vieira.leonardo@gmail.com>
2022-01-04 19:24:22 -0300

    refactor: alter .husky.json

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -3,7 +3,7 @@ export class SignUpController {
     if (httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: name')
+        body: new Error('Missing param: name alterado')
       }
     }
     if (!httpRequest.body.email) {
